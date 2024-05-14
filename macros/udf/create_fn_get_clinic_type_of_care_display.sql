55a48eb1eba52cc59b23dc3d1019f5063cc312c9
binrai <brai@dimagi.com>
2024-05-14 13:44:09 -0700

    remove conditional reference to schema and update quotes

--- macros/udf/create_fn_get_clinic_type_of_care_display.sql
+++ macros/udf/create_fn_get_clinic_type_of_care_display.sql
@@ -1,20 +1,6 @@
 {% macro create_fn_get_clinic_type_of_care_display() %}
 
-CREATE OR REPLACE FUNCTION 
-    {% if target.name=='dev' %}
-      DM_LADDERS_DEV.DM
-    {% elif target.name=='qa' %}
-      DM_LADDERS_QA.DM
-    {% elif target.name=='prod' %}
-      DM_LADDERS_PROD.DM
-    {% elif target.name=='test' %}
-      DM_LADDERS_TEST.DM
-    {% else %}
-      invalid
-    {% endif %}
-
--- .DM.FN_GET_CLINIC_TYPE_OF_CARE_DISPLAY(residential_services VARCHAR(), mental_health_settings varchar())
-.FN_GET_CLINIC_TYPE_OF_CARE_DISPLAY("residential_services" VARCHAR(16777216), "mental_health_settings" VARCHAR())
+CREATE OR REPLACE FUNCTION DM.FN_GET_CLINIC_TYPE_OF_CARE_DISPLAY("RESIDENTIAL_SERVICES" VARCHAR(16777216), "MENTAL_HEALTH_SETTINGS" VARCHAR(16777216))
 RETURNS VARCHAR(16777216)
 LANGUAGE SQL
 AS '{{ fn_get_clinic_type_of_care_display() }}';
--- macros/udf/fn_get_clinic_type_of_care_display.sql
+++ macros/udf/fn_get_clinic_type_of_care_display.sql
@@ -1,8 +1,8 @@
 {% macro fn_get_clinic_type_of_care_display() %}
     case 
-        when nvl(residential_services, "")  != "" and nvl(mental_health_settings, "")  != "" then "Both Mental Health & Substance Use"
-        when nvl(residential_services, "")  != "" and nvl(mental_health_settings, "")  = "" then "Substance Use"
-        when nvl(residential_services, "")  = "" and nvl(mental_health_settings, "")  != "" then "Mental Health"
-        when nvl(residential_services, "")  = "" and nvl(mental_health_settings, "")  = "" then "No information available"
+        when nvl(residential_services, '''')  != '''' and nvl(mental_health_settings, '''')  != '''' then ''Both Mental Health & Substance Use''
+        when nvl(residential_services, '''')  != '''' and nvl(mental_health_settings, '''')  = '''' then ''Substance Use''
+        when nvl(residential_services, '''')  = '''' and nvl(mental_health_settings, '''')  != '''' then ''Mental Health''
+        when nvl(residential_services, '''')  = '''' and nvl(mental_health_settings, '''')  = '''' then ''No information available''
     end
 {% endmacro %}
