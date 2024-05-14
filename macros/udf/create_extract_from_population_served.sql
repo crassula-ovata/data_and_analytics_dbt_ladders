ae0028fd3297da1d7ab893b1d0d60d3a6a38084e
binrai <brai@dimagi.com>
2024-05-14 10:52:55 -0700

    update macro

--- macros/udf/create_extract_from_population_served.sql
+++ macros/udf/create_extract_from_population_served.sql
@@ -1,6 +1,19 @@
 {% macro create_extract_from_population_served() %}
 
-CREATE OR REPLACE FUNCTION DM.EXTRACT_FROM_POPULATION_SERVED("POPULATION_SERVED_ORIGINAL" VARCHAR(16777216), "GENDER" BOOLEAN)
+CREATE OR REPLACE FUNCTION
+    {% if target.name=='dev' %}
+      DM_LADDERS_DEV
+    {% elif target.name=='qa' %}
+      DM_LADDERS_QA
+    {% elif target.name=='prod' %}
+      DM_LADDERS_PROD
+    {% elif target.name=='test' %}
+      DM_LADDERS_TEST
+    {% else %}
+      invalid
+    {% endif %}
+
+ DM.EXTRACT_FROM_POPULATION_SERVED("POPULATION_SERVED_ORIGINAL" VARCHAR(16777216), "GENDER" BOOLEAN)
     RETURNS VARCHAR(16777216)
     LANGUAGE PYTHON
     RUNTIME_VERSION = '3.8'
--- macros/udf/create_fn_get_clinic_type_of_care_display.sql
+++ macros/udf/create_fn_get_clinic_type_of_care_display.sql
@@ -1,17 +1,21 @@
 {% macro create_fn_get_clinic_type_of_care_display() %}
 
-CREATE OR REPLACE FUNCTION DM.FN_GET_CLINIC_TYPE_OF_CARE_DISPLAY(residential_services VARCHAR(), mental_health_settings varchar())
+CREATE OR REPLACE FUNCTION 
+    {% if target.name=='dev' %}
+      DM_LADDERS_DEV
+    {% elif target.name=='qa' %}
+      DM_LADDERS_QA
+    {% elif target.name=='prod' %}
+      DM_LADDERS_PROD
+    {% elif target.name=='test' %}
+      DM_LADDERS_TEST
+    {% else %}
+      invalid
+    {% endif %}
+
+.DM.FN_GET_CLINIC_TYPE_OF_CARE_DISPLAY(residential_services VARCHAR(), mental_health_settings varchar())
 RETURNS VARCHAR(16777216)
 LANGUAGE SQL
-AS 
-  $$
-    case 
-        when nvl(residential_services, '')  != '' and nvl(mental_health_settings, '')  != '' then 'Both Mental Health & Substance Use'
-        when nvl(residential_services, '')  != '' and nvl(mental_health_settings, '')  = '' then 'Substance Use'
-        when nvl(residential_services, '')  = '' and nvl(mental_health_settings, '')  != '' then 'Mental Health'
-        when nvl(residential_services, '')  = '' and nvl(mental_health_settings, '')  = '' then 'No information available'
-    end
-  $$
-;
+AS '{{ fn_get_clinic_type_of_care_display() }}';
 
 {% endmacro %}
