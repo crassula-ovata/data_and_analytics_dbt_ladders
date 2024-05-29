34f69201572ac8a89d9da46903fbfdae8260f023
binrai <brai@dimagi.com>
2024-05-29 11:44:21 -0700

    additional singular tests

---
+++ tests/check_clinic_case_name_not_duplicate_creation.sql
@@ -0,0 +1,11 @@
+with 
+dm_table_data_clinic_open as (
+    select * from  {{ source('dm_table_data', 'CASE_CLINIC') }}
+    where closed = false 
+)
+
+select * 
+from  VW_CLINICS_CREATE_UPDATE 
+where
+    action = 'create'
+    and case_name in (select case_name from dm_table_data_clinic_open)
