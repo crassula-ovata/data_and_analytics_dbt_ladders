36776e4d527bfbe8f5e754c43bb8e54a42a64330
binrai <brai@dimagi.com>
2024-07-18 08:48:00 -0700

    Additional functions to handle for bhe-clinics

---
+++ macros/udf/fn_get_substance_use_services.sql
@@ -0,0 +1,20 @@
+{% macro fn_get_substance_use_services() %}
+
+    TRIM(CONCAT('' '', 
+            IFF(OUTPATIENT, ''outpatient '', ''''),
+            IFF(INTENSIVE_OUTPATIENT_SU_SERVICES, ''intensive_outpatient '', ''''),
+            IFF(GENERAL_TREATMENT, ''general_treatment '', ''''),
+            IFF(DUI_DWI, ''dui_dwi '', ''''),
+            IFF(EDU_TTMT_SVCS_FOR_PERSONS_IN_CJS, ''education_and_treatment_services_for_persons_in_criminal_justice_system '', ''''),
+            IFF(CLINIC_MANAGED_LOW_INTENSE_RES_SVCS, ''clinically_managed_low_intensity_residential_services '', ''''),
+            IFF(CLINIC_MANAGED_MED_INTENSE_RES_SVCS, ''clinically_managed_medium_intensity_residential_services '', ''''),
+            IFF(CLINIC_MANAGED_HIGH_INTENSE_RES_SVCS, ''clinically_managed_high_intensity_residential_services '', ''''),
+            IFF(CLINIC_MANAGED_RESIDENTIAL_DETOX, ''clinically_managed_residential_detoxification '', ''''),
+            IFF(OPIOID_TREATMENT_PROGRAMS, ''opioid_treatment_programs '', ''''),
+            IFF(GENDER_RESPONSIVE_TTMT_FOR_WOMEN, ''gender_responsive_treatment_for_women '', ''''),
+            IFF(DAY_TREATMENT_PARTIAL_HOSPITALIZATION, ''day_treatment_partial_hospitalization '', ''''),
+            IFF(MED_MONITORED_INPATIENT_DETOX, ''medically_monitored_inpatient_detoxification '', ''''),
+            IFF(MEDICALLY_MONITORED_INTENSE_RES_TRTMT, ''medically_monitored_intensive_residential_treatment '', '''')
+        )) 
+
+{% endmacro %}
