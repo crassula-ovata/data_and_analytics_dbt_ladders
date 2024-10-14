d357c9e6a2dd0045be7bab1417258de863e617a7
binrai <brai@dimagi.com>
2024-10-14 13:51:15 -0700

    test for 0 lat long values

--- tests/check_clinic_lat_long_values.sql
+++ tests/check_clinic_lat_long_values.sql
@@ -1,7 +1,7 @@
 select * 
 from  {{ ref('VW_CLINICS_CREATE_UPDATE')}} 
 where
-    latitude = 0 
-    or longitude = 0 
-    or split_part(map_coordinates, ' ', 1) = 0 
-    or split_part(map_coordinates, ' ', 2) = 0 
+    TRY_TO_NUMBER(latitude, 6) = 0 
+    or TRY_TO_NUMBER(longitude, 6) = 0 
+    or  TRY_TO_NUMBER(split_part(map_coordinates, ' ', 1), 6) = 0 
+    or TRY_TO_NUMBER(split_part(map_coordinates, ' ', 2), 6) = 0
