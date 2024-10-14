55847d9514f97bc27cbcf50738cd4d1d87bfd37c
binrai <brai@dimagi.com>
2024-10-14 13:51:04 -0700

    improved logic

--- macros/udf/fn_validate_long.sql
+++ macros/udf/fn_validate_long.sql
@@ -1,12 +1,13 @@
 {% macro fn_validate_long() %}
 
 case 
-    when longitude is not null and 
-         longitude != '' and 
-         TRY_TO_NUMBER(longitude, 6) between -180 and 180 and 
-         TRY_TO_NUMBER(longitude, 6) != 0 
+    when 
+        longitude is not null and 
+        TRY_TO_NUMBER(longitude, 6) is not null and 
+        (TRY_TO_NUMBER(longitude, 6) between -180 and  180) and 
+        TRY_TO_NUMBER(longitude, 6) != 0
     then longitude
     else null 
 end
-  
+
 {% endmacro %}
