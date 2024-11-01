b5ce29e04420f04f876d7e2298437f1016773be0
binrai <brai@dimagi.com>
2024-11-01 09:44:03 -0700

    update logic for fn_format_referral_type

--- macros/udf/fn_format_referral_type.sql
+++ macros/udf/fn_format_referral_type.sql
@@ -1,7 +1,20 @@
-{% fn_format_referral_type %} macro  
+{% macro fn_format_referral_type() %}   
 
-    regexp_replace(
-            replace(replace(replace(replace(replace(coalesce(REFERRAL_TYPE, ''N/A''), ''court_mandated_referrals_only'', ''Court Mandated Referrals Only,''), ''insurance_authorization'', ''Insurance Pre-Authorization,''), ''physical_assessments'', ''Physical Assessments,''),''psychiatric_assessments'', ''Psychiatric Assessments,''), ''walk_in'', ''Walk-In,'')
-            , '',$'', '''')
+fromat_map = {
+    "court_mandated_referrals_only": "Court Mandated Referrals Only",
+    "insurance_authorization": "Insurance Pre-Authorization",
+    "physical_assessments": "Physical Assessments",
+    "psychiatric_assessments": "Psychiatric Assessments",
+    "walk_in": "Walk-In"
+}
+
+def fn_format_referral_type(REFERRAL_TYPE):
+    if not REFERRAL_TYPE:
+        REFERRAL_TYPE = "N/A"
+    else: 
+        for default_value, formated_value in fromat_map.items():
+            REFERRAL_TYPE = REFERRAL_TYPE.replace(default_value, formated_value)
+        REFERRAL_TYPE = REFERRAL_TYPE.rstrip(",")
+    return REFERRAL_TYPE
 
 {% endmacro %}
