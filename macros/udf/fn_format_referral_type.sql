eacfc5db1d5a19e7aa9fea42ef560c2658f43b1b
binrai <brai@dimagi.com>
2024-11-05 08:19:54 -0800

    replace clinic_referral_type with clinic_referral_type_display and include N/A

--- macros/udf/fn_format_referral_type.sql
+++ macros/udf/fn_format_referral_type.sql
@@ -1,6 +1,6 @@
 {% macro fn_format_referral_type() %}   
 
-fromat_map = {
+format_map = {
     "court_mandated_referrals_only": "Court Mandated Referrals Only,",
     "insurance_authorization": "Insurance Pre-Authorization,",
     "physical_assessments": "Physical Assessments,",
@@ -10,11 +10,12 @@ fromat_map = {
 
 def fn_format_referral_type(REFERRAL_TYPE):
     if not REFERRAL_TYPE:
-        REFERRAL_TYPE = "N/A"
-    else: 
-        for default_value, formated_value in fromat_map.items():
+        return REFERRAL_TYPE 
+    else:
+        for default_value, formated_value in format_map.items():
             REFERRAL_TYPE = REFERRAL_TYPE.replace(default_value, formated_value)
         REFERRAL_TYPE = REFERRAL_TYPE.rstrip(",")
+
     return REFERRAL_TYPE
 
 {% endmacro %}
